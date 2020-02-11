b67c4e3ee73cd2d08bba18f6dd156b0babe20287
Naoto Shimazaki <naoto.shimazaki@gmail.com>
2020-02-12 03:32:46 +0900

    fix garbage in comments

--- hexdump.h
+++ hexdump.h
@@ -22,9 +22,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
-
- * MVar is one element only thread safe queue.
- * This is MVar implementation in C and pthread.
  */
 
 #include <stddef.h>
--- mvar.c
+++ mvar.c
@@ -20,11 +20,12 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
-
- * MVar is one element only thread safe queue.
- * This is MVar implementation in C and pthread.
  */
 
+//
+//  MVar is one element only thread safe queue.
+//  This is MVar implementation in C and pthread.
+//
 #include <errno.h>
 #include <time.h>
 
